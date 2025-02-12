94ab90b28c32d81858bca788fec70224f48b21f8
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-11 19:02:47 -0800

    small edit

--- src/store/articleReducer.js
+++ src/store/articleReducer.js
@@ -1,6 +1,7 @@
 import articles from '../data/data.json';
 
 const LOAD_ARTICLES = 'article/loadArticles';
+const ADD_ARTICLE = 'article/addArticle';
 
 export const loadArticles = () => {
   return {
@@ -9,12 +10,21 @@ export const loadArticles = () => {
   };
 };
 
+export const addArticle = (article) => {
+  return {
+    type: ADD_ARTICLE,
+    article
+  }
+}
+
 const initialState = { entries: [], isLoading: true };
 
 const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
       return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      return { ...state, entries: [...action.articles, action.article] }
     default:
       return state;
   }
